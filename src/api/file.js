c45bfee3a05e5918ded8fb42e77e3d0a63609be6
guyang1024 <2603001448@qq.com>
2026-01-20 01:48:14 +0800

    feat: 上传图片的接口

---
+++ src/api/file.js
@@ -0,0 +1,18 @@
+import request from '@/utils/request'
+
+// 上传文件
+export const uploadFileAPI = (files) => {
+  const formData = new FormData()
+  // files 是一个数组
+  files.forEach((file) => {
+    formData.append('files', file)
+  })
+  return request({
+    url: '/admin/v1/file/upload',
+    method: 'post',
+    data: formData,
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  })
+}
